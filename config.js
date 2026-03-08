360b30351b5bc5549324704f1f240188f0e17817
Mzazi-tech <mzazitech001@gmail.com>
2026-03-09 02:40:09 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 module.exports = {
   // Bot Identity
-  BOT_NAME: "Ultra X",
+  BOT_NAME: "CyberByte Ai",
   VERSION: "5.0.0",
 
   // Core Settings
   PREFIX: ".",
 
   // Owners (without + or spaces)
-  OWNER: ["254788460896"],
+  OWNER: ["254741388986"],
 
   // Sticker Settings
-  PACK_NAME: "Trashcore Stickers",
+  PACK_NAME: "CyberTech Ai",
   AUTHOR: "Trashcore",
 
   // Database — set via env var DATABASE_URL (recommended) or paste here
   DATABASE_URL: "",
 
   // Panel password — set via env var PANEL_PASSWORD or here
-  PANEL_PASSWORD: "admin123",
+  PANEL_PASSWORD: "4224@a",
 
   // Max allowed sessions (default 30)
   MAX_SESSIONS: 30
