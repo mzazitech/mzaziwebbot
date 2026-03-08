332396f0dc3459fc786ff697fc531dae2c29b56c
Mzazi-tech <mzazitech001@gmail.com>
2026-03-08 18:02:56 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,28 @@
+export default function Home(){
+
+return(
+
+<div style={{padding:"40px"}}>
+
+<h1>Mzazi Bot Hosting</h1>
+
+<p>
+Connect your WhatsApp bot easily.
+</p>
+
+<a href="/connect">
+<button style={{
+padding:"12px 20px",
+background:"black",
+color:"white",
+border:"none"
+}}>
+Connect Bot
+</button>
+</a>
+
+</div>
+
+)
+
+}
