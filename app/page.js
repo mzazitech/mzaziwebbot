0067b5f2b52ba2678fc3c5b037b83c0868cc629b
Mzazi-tech <mzazitech001@gmail.com>
2026-03-09 13:41:01 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+
+export default function Home() {
+  const { userId } = auth()
+  if (userId) {
+    redirect('/dashboard')
+  } else {
+    redirect('/sign-in')
+  }
+}
