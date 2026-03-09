55f4336d5b45a7922d83f0637ee78781edefa709
Mzazi-tech <mzazitech001@gmail.com>
2026-03-09 14:36:43 +0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',  // replaces `next export`
-  images: {
-    unoptimized: true, // required if you use next/image (Render's static hosting doesn't support image optimization)
-  },
-};
-
-module.exports = nextConfig;
